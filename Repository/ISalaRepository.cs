8712d837724914bf3cf8f1451aef705c8748b96a
HMarchiori <hmarchiori2003@gmail.com>
2025-08-25 15:42:19 -0300

    FEAT: Create Repository Interfaces for ILaboratorioRepository.cs and ISalaRepository.cs.

---
+++ Repository/ISalaRepository.cs
@@ -0,0 +1,11 @@
+using backend.Dto.Sala;
+using backend.Models;
+
+namespace backend.Repository;
+
+public interface ISalaRepository
+{
+    public Task<List<Sala>> GetAllAsync();
+    public Task<Sala> GetByIdAsync(long id);
+    public Task<Sala> UpdateAsync(SalaDto dto);
+}
